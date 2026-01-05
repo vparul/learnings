c85f51c687e1b02eb98242c22db4944e964d5e3e
vparul <verma.parul14799@gmail.com>
2026-01-05 16:24:26 +0530

    fixing directories

--- learnings/General/1.js
+++
@@ -1,8 +0,0 @@
-// // A high level language is a language which is easy for human to read abd write without worrying about how the tasks are
-// perfomed in the background.
-
-// It is like writing console.log("Hello"). Here, you just told the computer what to do without worrying about how it would be done.
-
-// Whereas, in low level language, you hav to give the exact instructions to the computer to perform any action.
-
-// Like, in high level, let's say you are giving intruction to a robot to make tea but in low level, you have to exactly say take vessel, turn stove, put wanter in the vessel, add tea boil
